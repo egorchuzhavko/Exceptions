e7fec20bc85b2887a0b6b1a349aba95d4815e3e0
jegorjegor <egorchuzhavko@gmail.com>
2021-12-20 09:10:03 +0300

    end

--- part1/Program.cs
+++ part1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace part1
 {
@@ -6,11 +7,15 @@ namespace part1
     {
         static void Main(string[] args)
         {
+            //задание 1
             double? a, b, c, p = null;
             try
             {
+                Console.Write("Vvedite a: ");
                 a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Vvedite b: ");
                 b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Vvedite c: ");
                 c = Convert.ToDouble(Console.ReadLine());
                 if (a == 0 | b == 0 | c == 0)
                     throw new Exception("Одна или несколько сторон равны нулю");
@@ -26,8 +31,46 @@ namespace part1
             {
                 Console.WriteLine(ex.Message);
             }
-
             Console.WriteLine("\n" + p);
+            
+            //задание 2
+            int? n = null;
+            try
+            {
+                Console.Write("Vvedite razmernost' massiva: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                if (n <= 0)
+                    throw new Exception("n <= 0"); 
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Neverniy format dannih");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            int[,] mas = new int[Convert.ToInt32(n), Convert.ToInt32(n)];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    mas[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            bool flag = true;
+            for (int i = 0; i < n & flag; i++)
+            {
+                for (int j = 0; j < n & flag; j++)
+                {
+                    if (mas[i, j] != mas[j, i])
+                        flag = false;
+                }
+            }
+
+            Console.WriteLine(flag ? "Матрица симметрична" : "Матрица не симметрична");
         }
     }
 }
